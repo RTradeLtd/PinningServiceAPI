9d4cbc9b0912a1ba7f4a2bd58936c39f4e9768ef
Alex <alext@rtradetechnologies.com>
2019-08-08 02:10:13 -0700

    cmd: update test binary

--- cmd/psapi/main.go
+++ cmd/psapi/main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"flag"
+	"log"
+	"time"
+
 	"github.com/RTradeLtd/PinningServiceAPI/api"
+	"github.com/RTradeLtd/rtfs/v2"
 )
 
+var (
+	ipfsURL = flag.String("ipfs.url", "127.0.0.1:5001", "ipfs http api endpoint")
+)
+
+func init() {
+	flag.Parse()
+}
+
 func main() {
-	router := api.NewAPI()
+	manager, err := rtfs.NewManager(*ipfsURL, "", time.Hour)
+	if err != nil {
+		log.Fatal(err)
+	}
+	router := api.NewAPI(manager)
 	router.Run(":8080")
 }
